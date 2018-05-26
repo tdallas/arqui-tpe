3b5087a29db0ad3d7b56c4f94ac79d09611e5dd1
tdallas <tdallas@itba.edu.ar>
2018-05-26 17:25:09 -0300

    Hice isNum e isChar, habria que ver si se puede refactorizar el isNum para hacerlo un toque mas lindo

--- Userland/SampleCodeModule/stdlib.c
+++ Userland/SampleCodeModule/stdlib.c
@@ -1,6 +1,8 @@
 //COMPLETAR .h CON CADA FUNCION
 #include <stdlib.h>
 
+#define IS_DIGIT(character) ((character - '0') >= 0 && (character - '0') <= 9)
+
 unsigned long int rand_next = 1;
 
 /*
@@ -23,4 +25,49 @@ int rand()
 void srand(unsigned int seed)
 {
 	rand_next = seed;
+}
+
+int isChar(char *c) {
+	return IS_DIGIT(*c) ? 0 : 1;
+}
+
+// Parameters: Char pointer
+// Return values: 2 if float, 1 if int, 0 if not num
+//fixme estaria bueno hacer un refactor
+int isNum(char *string)
+{
+
+	int isFloat = 0;
+	//Check wether it could by negative number
+	if (*string == '-')
+	{
+		s++;
+	}
+
+	if (!IS_DIGIT(*string))
+	{
+		return 0;
+	}
+	s++;
+
+	while (*string != '\0')
+	{
+		int decimal_marks = 0;		
+		if (!IS_DIGIT(*string))
+		{
+			if (*string == '.')
+				isFloat++;
+			if (*string == '.' && decimal_marks == 0 && IS_DIGIT(*(string + 1)))
+			{
+				decimal_marks++;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		s++;
+	}
+
+	return 1 + isFloat;
 }
